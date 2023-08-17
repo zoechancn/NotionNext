dd5320e8e21c8f8e7890d3d43f97e66b9822088a
zoechancn <zoechancn@163.com>
2023-08-17 13:14:52 +0800

    config.js

--- themes/heo/config.js
+++ themes/heo/config.js
@@ -6,7 +6,7 @@ const CONFIG = {
   // 首页顶部通知条滚动内容，如不需要可以留空 []
   NOTICE_BAR: [
     { title: '欢迎来到我的博客', url: 'https://blog.zoeserver.top' },
-  ],
+  ], 
 
   // 英雄区(首页顶部大卡)
   HERO_TITLE_1: 'unRaid',
